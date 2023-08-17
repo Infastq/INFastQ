70f42ab8683e07ec76d853738e867caf2a62c97c
ZeevHaydar <nzhifhaidar@gmail.com>
2023-08-17 15:18:00 +0700

    Update main.cpp
    commit tes

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,31 @@
 #include <Arduino.h>
 
+#define S0 33
+#define S1 32
+#define Out 27
+#define S2 26
+#define S3 25
+
+// Stores frequency read by the photodiodes
+int redFrequency = 0;
+int greenFrequency = 0;
+int blueFrequency = 0;
+
 // put function declarations here:
 int myFunction(int, int);
 
+
 void setup() {
   // put your setup code here, to run once:
+  pinMode(S0, OUTPUT);
+  pinMode(S1, OUTPUT);
+  pinMode(S2, OUTPUT);
+  pinMode(S3, OUTPUT);
+
+  // Setting the sensorOut as an input
+  pinMode(Out, INPUT);
+
+
   int result = myFunction(2, 3);
 }
 
